2a590360ba73824375c77264ddeaa70bd85a0d83
TanyaChichkan <TanyaChichkan@users.noreply.github.com>
2021-02-02 13:21:34 +0200

    with TK

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { createStore } from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
-import {phoneBookReducer} from './reducer/contactsReducer';
+import {rootReducer} from './reducer/contactsReducer';
+import {configureStore} from '@reduxjs/toolkit';
 
-const store = createStore(phoneBookReducer, composeWithDevTools());
+// const store = createStore(phoneBookReducer, composeWithDevTools());
+
+const store = configureStore({
+  reducer: rootReducer
+});
 
 export default store;
